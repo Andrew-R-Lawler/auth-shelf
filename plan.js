b2a17f73efe3a18ce816530df38660591659f433
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-25 16:50:46 -0500

    insrtructions

---
+++ plan.js
@@ -0,0 +1,11 @@
+//GET(Nai)(client, index, server)
+//POST (Jayden)(client, index, server)
+//DELETE (Andrew & Jay)(client, index, server)
+//Authentication (Andrew & Jay)
+
+
+//INFOPAGE
+//Map reduxState
+//Add item (form for creating a new item to shelf)
+//DELETE button(only functional for that user/ authenticated)
+
