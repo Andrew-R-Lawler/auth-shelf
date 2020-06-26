834e1f64e504e0c21775c3a95014b53038abface
Andrew Lawler <Andrew.R.Lawler@gmail.com>
2020-06-26 10:36:11 -0500

    shelf saga structure

--- src/redux/sagas/shelfSaga.js
+++ src/redux/sagas/shelfSaga.js
@@ -1,2 +1,17 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
+import { response } from 'express';
+
+function* getItems() {
+    try {
+        const itemsResponse = yield axios.get('/api/shelf')
+        yield put({ type: 'SET_SHELF', payload: response.data })
+    } catch (error) {
+        console.log('error', error );
+    }
+}
+function* shelfSaga () {
+    yield takeLatest('GET_SHELF', getItems)
+}
+
+export default shelfSaga;
