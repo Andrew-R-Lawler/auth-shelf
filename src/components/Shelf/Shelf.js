a6234cb7ff4305f6161e7d8d673f5e2222722a6a
Your Name <nailaj123@gmail.com>
2020-06-26 10:37:11 -0500

    added client/server get request

--- src/components/Shelf/Shelf.js
+++ src/components/Shelf/Shelf.js
@@ -1,14 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class Shelf extends Component{
+class Shelf extends Component {
     //GET
-
-    render(){
-        return(
+    componentDidMount() {
+        this.getItems();
+    }
+    getItems() {
+        const action = { type: 'GET_ITEMS' };
+        this.props.dispatch(action);
+    };
+    render() {
+        return (
             <h1>Shelf</h1>
 
         )//end return
     }//end render
 }//end class
 
-export default Shelf;
+const mapReduxStoreToProps = (reduxStore) => ({
+    reduxStore: reduxStore
+})
+
+export default connect(mapReduxStoreToProps)(Shelf);
