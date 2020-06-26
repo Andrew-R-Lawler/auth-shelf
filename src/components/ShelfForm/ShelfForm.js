4e0ee488a28ab1f1f62c6601b6568c9564beb6a2
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-26 10:21:55 -0500

    inputs and button

--- src/components/ShelfForm/ShelfForm.js
+++ src/components/ShelfForm/ShelfForm.js
@@ -1,14 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
-class ShelfForm extends Component{
+class ShelfForm extends Component {
     //POST w/inputs
+    state = {
+        item: ''
+    }
+    newItem = (event) => {
+        console.log('input is:', event.target.value);
+        this.setState({
+            item: event.target.value,
+        })
+    }//end newItem
 
-    render(){
-        return(
-            <h1>ShelfForm</h1>
+    addItem = () => {
+        console.log('add this item:', this.state.item)
+        this.props.dispatch({
+            type: 'ADD_ITEM',
+            payload: this.state.item
+        })
+    }//end addItem
 
+    render() {
+        return (
+            <div>
+                <h1>ShelfForm</h1>
+                <input placeholder="Add new item" onChange={this.newItem}></input>
+                <button onClick={this.addItem}>Add Item</button>
+            </div>
         )//end return
     }//end render
 }//end class
 
-export default ShelfForm;
+export default connect()(ShelfForm);
